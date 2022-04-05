f96741a137ed34704da2f84b84abed5c8e65a531
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-05 13:28:08 +0900

    0405

--- webDesign/js/site.js
+++ webDesign/js/site.js
@@ -4,3 +4,12 @@ $("#header > nav > ul > li").mouseover(()=>{
 $("#header > nav > ul > li").mouseout(()=>{
     $("#header > nav > ul > li").find(".hid").stop().slideUp();
 })
+
+$(".imgBox > h2 > a").click(()=>{
+    $(".imgBox .best").addClass("active")
+    $(".imgBox .comment").removeClass("active")
+})
+$(".imgBox > span > a").click(()=>{
+    $(".imgBox .comment").addClass("active")
+    $(".imgBox .best").removeClass("active")
+})
