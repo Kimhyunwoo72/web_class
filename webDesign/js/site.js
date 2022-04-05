049200964b7020c0300524fd6d45c04e18daf437
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-05 14:03:08 +0900

    0405

--- webDesign/js/site.js
+++ webDesign/js/site.js
@@ -13,3 +13,31 @@ $(".imgBox > span > a").click(()=>{
     $(".imgBox .comment").addClass("active")
     $(".imgBox .best").removeClass("active")
 })
+
+   // slider
+   const sliderWrap = document.querySelector(".slider__wrap")
+   const sliderImg = document.querySelector(".slider__img")
+   const sliderInner = document.querySelector(".slider__inner")
+   const slider = document.querySelectorAll(".slider")
+
+   let currentIndex = 0;
+   let sliderCount = slider.length //이미지 갯수
+   let sliderWidth = sliderImg.offsetWidth //이미지 가로값
+   let sliderClone = sliderInner.firstElementChild.cloneNode(true) //첫번째 이미지 복사
+   sliderInner.appendChild(sliderClone) //첫번째 이미지를 마지막에 넣어줌
+   
+   function sliderEffect() {
+       currentIndex++;
+       sliderInner.style.transition = "all 0.3s";  
+       sliderInner.style.transform = "translateX(-" + sliderWidth * currentIndex + "px)"
+       //마지막 사진에 갔을 때
+       if(currentIndex == sliderCount){
+           setTimeout(() => {
+               //초기화
+               sliderInner.style.transition = "0s";
+               sliderInner.style.transform = "translateX(0px)"
+           },300)
+           currentIndex = 0;
+       }
+   }
+   setInterval(sliderEffect, 2000);
