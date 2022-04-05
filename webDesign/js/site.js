b88536da1e811f1b241c1ea18b861a7312dc384f
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-05 12:43:21 +0900

    20220405

---
+++ webDesign/js/site.js
@@ -0,0 +1,6 @@
+// $("#header > nav > ul > li").mouseover(()=>{
+//     $("#header > nav > ul > li").find(".hid").stop().slideDown();
+// })
+// $("#header > nav > ul > li").mouseout(()=>{
+//     $("#header > nav > ul > li").find(".hid").stop().slideUp();
+// })
