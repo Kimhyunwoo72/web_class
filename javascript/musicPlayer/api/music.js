93b6655ee916c7ab734c8d641f7f42a431bd1c14
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-08 13:06:58 +0900

    0408

--- javascript/musicPlayer/api/music.js
+++ javascript/musicPlayer/api/music.js
@@ -1,68 +1,63 @@
 let allMusic = [
     {
-        name: "Sailing",
+        name: "1. Sailing",
         artist : "Telecasted",
         img : "music01",
         audio : "music01"
     },   
     {
-        name: "Breeze",
+        name: "2. Breeze",
         artist : "Telecasted",
         img : "music02",
         audio : "music02"
     },   
     {
-        name: "Sunshine",
+        name: "3. Sunshine",
         artist : "jone Mickal",
         img : "music03",
         audio : "music03"
     },   
     {
-        name: "Built to Last (Instrumental)",
+        name: "4. Built to Last (Instrumental)",
         artist : "NEFFEX",
         img : "music04",
         audio : "music04"
     },   
     {
-        name: "Hustlin' (Instrumental)",
+        name: "5. Hustlin' (Instrumental)",
         artist : "NEFFEX",
         img : "music05",
         audio : "music05"
     },   
     {
-        name: "Bop Walker",
+        name: "6. Bop Walker",
         artist : "Freedom Trail Studio",
         img : "music06",
         audio : "music06"
     },   
     {
-        name: "Robots and Aliens",
+        name: "7. Robots and Aliens",
         artist : "Joel Cummins",
         img : "music07",
         audio : "music07"
     },   
     {
-        name: "노래제목입니다.",
+        name: "8. 노래제목입니다.",
         artist : "jone Mickal",
         img : "music08",
         audio : "music08"
     },   
     {
-        name: "노래제목입니다.",
+        name: "9. 노래제목입니다.",
         artist : "jone Mickal",
         img : "music09",
         audio : "music09"
     },   
     {
-        name: "노래제목입니다.",
+        name: "10. 노래제목입니다.",
         artist : "jone Mickal",
         img : "music10",
         audio : "music10"
     },   
-    {
-        name: "노래제목입니다.",
-        artist : "jone Mickal",
-        img : "music11",
-        audio : "music11"
-    },   
+
 ]
