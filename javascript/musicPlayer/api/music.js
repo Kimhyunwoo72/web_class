ac37f34ab434dfdee23838620e2c944a9be3f022
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-08 09:30:03 +0900

    0408

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,68 @@
+let allMusic = [
+    {
+        name: "Sailing",
+        artist : "Telecasted",
+        img : "music01",
+        audio : "music01"
+    },   
+    {
+        name: "Breeze",
+        artist : "Telecasted",
+        img : "music02",
+        audio : "music02"
+    },   
+    {
+        name: "Sunshine",
+        artist : "jone Mickal",
+        img : "music03",
+        audio : "music03"
+    },   
+    {
+        name: "Built to Last (Instrumental)",
+        artist : "NEFFEX",
+        img : "music04",
+        audio : "music04"
+    },   
+    {
+        name: "Hustlin' (Instrumental)",
+        artist : "NEFFEX",
+        img : "music05",
+        audio : "music05"
+    },   
+    {
+        name: "Bop Walker",
+        artist : "Freedom Trail Studio",
+        img : "music06",
+        audio : "music06"
+    },   
+    {
+        name: "Robots and Aliens",
+        artist : "Joel Cummins",
+        img : "music07",
+        audio : "music07"
+    },   
+    {
+        name: "노래제목입니다.",
+        artist : "jone Mickal",
+        img : "music08",
+        audio : "music08"
+    },   
+    {
+        name: "노래제목입니다.",
+        artist : "jone Mickal",
+        img : "music09",
+        audio : "music09"
+    },   
+    {
+        name: "노래제목입니다.",
+        artist : "jone Mickal",
+        img : "music10",
+        audio : "music10"
+    },   
+    {
+        name: "노래제목입니다.",
+        artist : "jone Mickal",
+        img : "music11",
+        audio : "music11"
+    },   
+]
