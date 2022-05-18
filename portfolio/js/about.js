d7c1c2680ade554bac0fbcd739921a0c0beb99f4
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-05-18 13:50:52 +0900

    0518

--- portfolio/js/about.js
+++ portfolio/js/about.js
@@ -1,51 +1,589 @@
-   $(document).ready(function () {
-      var curPage = 1;
-      var numOfPages = $(".skw-page").length;
-      var animTime = 1000;
-      var scrolling = false;
-      var pgPrefix = ".skw-page-";
+var windowWidth = window.innerWidth;
+if (windowWidth <= 750) {
+  gsap.to(".about_page", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      toggleActions: "resume pause reset pause",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    duration: 1,
+  });
 
-      function pagination() {
-        scrolling = true;
+  // about_title
+  gsap.from(".about_title h2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    y: 150,
+    duration: 1,
+  });
+  gsap.to(".about_title h2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+  });
+  // about_title
 
-        $(pgPrefix + curPage)
-          .removeClass("inactive")
-          .addClass("active");
-        $(pgPrefix + (curPage - 1)).addClass("inactive");
-        $(pgPrefix + (curPage + 1)).removeClass("active");
+  // about_num
+  gsap.from(".about_cont ul li .about_num1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    x: -100,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_num1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 0.7,
+    ease: "sine.out",
+    delay: 0.8,
+  });
 
-        setTimeout(function () {
-          scrolling = false;
-        }, animTime);
+  gsap.from(".about_cont ul li .about_num2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    x: -100,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_num2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 0.7,
+    ease: "sine.out",
+    delay: 1.2,
+  });
+
+  gsap.from(".about_cont ul li .about_num3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    x: -100,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_num3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 0.7,
+    ease: "sine.out",
+    delay: 1.5,
+  });
+  // about_num
+
+  //about_desc
+  gsap.from(".about_cont ul li .about_desc1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    y: 20,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_desc1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power1.out",
+    delay: 1.4,
+  });
+
+  gsap.from(".about_cont ul li .about_desc2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    y: 20,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_desc2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power1.out",
+    delay: 1.6,
+  });
+
+  gsap.from(".about_cont ul li .about_desc3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    y: 20,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_desc3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power1.out",
+    delay: 1.8,
+  });
+  //about_desc
+} else {
+  gsap.to(".about_page", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      toggleActions: "resume pause reset pause",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    duration: 1,
+  });
+
+  // about_title
+  gsap.from(".about_title h2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    y: 100,
+    duration: 1,
+  });
+  gsap.to(".about_title h2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1,
+  });
+  // about_title
+
+  // about_cont
+  gsap.from(".about_cont", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    width: 0,
+  });
+  gsap.to(".about_cont", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    width: 100 + "%",
+    duration: 1,
+  });
+  // about_cont
+
+  //about_cont line
+  gsap.from(".about_cont ul li:nth-child(1)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    height: 0,
+  });
+  gsap.to(".about_cont ul li:nth-child(1)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    height: 100 + "%",
+    duration: 1,
+    ease: "none",
+    delay: 1,
+  });
+  //about_cont line
+  gsap.from(".about_cont ul li > div", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    scaleX: 1,
+    scaleY: 1,
+  });
+  gsap.to(".about_cont ul li > div", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    delay: 1,
+    duration: 4,
+    scaleX: 1.3,
+    scaleY: 1.3,
+  });
+
+  gsap.from(".about_cont ul li:nth-child(2)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    height: 0,
+  });
+  gsap.to(".about_cont ul li:nth-child(2)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    height: 100 + "%",
+    duration: 0.7,
+    ease: "none",
+    delay: 1.1,
+  });
+
+  gsap.from(".about_cont ul li:nth-child(3)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    height: 0,
+  });
+  gsap.to(".about_cont ul li:nth-child(3)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    height: 100 + "%",
+    duration: 0.7,
+    ease: "none",
+    delay: 1.2,
+    paddingTop: 9 + "%",
+  });
+
+  gsap.from(".about_cont ul li:nth-child(4)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 0,
+    height: 0,
+  });
+  gsap.to(".about_cont ul li:nth-child(4)", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    height: 100 + "%",
+    duration: 0.7,
+    ease: "none",
+    delay: 1.4,
+    paddingTop: 15 + "%",
+  });
+  //about_cont line
+
+  // about_num
+  gsap.from(".about_cont ul li .about_num1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    x: -100,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_num1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 0.7,
+    ease: "sine.out",
+    delay: 1.8,
+  });
+
+  gsap.from(".about_cont ul li .about_num2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    x: -100,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_num2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 0.7,
+    ease: "sine.out",
+    delay: 2.2,
+  });
+
+  gsap.from(".about_cont ul li .about_num3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    x: -100,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_num3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    x: 0,
+    duration: 0.7,
+    ease: "sine.out",
+    delay: 2.5,
+  });
+  // about_num
+
+  //about_desc
+  gsap.from(".about_cont ul li .about_desc1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    y: 20,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_desc1", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power1.out",
+    delay: 2.4,
+  });
+
+  gsap.from(".about_cont ul li .about_desc2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    y: 20,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_desc2", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power1.out",
+    delay: 2.6,
+  });
+
+  gsap.from(".about_cont ul li .about_desc3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    y: 20,
+    opacity: 0,
+  });
+  gsap.to(".about_cont ul li .about_desc3", {
+    scrollTrigger: {
+      trigger: ".about_page",
+      markers: true,
+      start: "5% center",
+    },
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power1.out",
+    delay: 2.8,
+  });
+  //about_desc
+}
+
+/////////////////////////////////////////////////////////////////
+
+//about_page 2 title-->
+gsap.from(".aboutImg_box > .about_text", {
+  scrollTrigger: {
+    trigger: ".aboutImg_box",
+    markers: true,
+    start: "10% center",
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1,
+});
+gsap.to(".aboutImg_box > .about_text", {
+  scrollTrigger: {
+    trigger: ".aboutImg_box",
+    markers: true,
+    start: "10% center",
+  },
+  opacity: 1,
+  y: 0,
+  duration: 1,
+});
+//about_page 2 title-->
+
+//about_page 2 img-->
+gsap.from(".slider__img", {
+  scrollTrigger: {
+    trigger: ".aboutImg_box",
+    markers: true,
+    start: "10% center",
+  },
+  opacity: 0,
+  duration: 1,
+});
+gsap.to(".slider__img", {
+  scrollTrigger: {
+    trigger: ".aboutImg_box",
+    markers: true,
+    start: "10% center",
+  },
+  opacity: 1,
+  ease: "power2.out",
+  duration: 2,
+});
+//about_page 2 img-->
+
+// slider
+var menu = [];
+jQuery(".swiper-slide").each(function (index) {
+  menu.push(jQuery(this).find(".slide-inner").attr("data-text"));
+});
+var interleaveOffset = 0.5;
+var swiperOptions = {
+  loop: true,
+  speed: 1000,
+  parallax: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  watchSlidesProgress: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  on: {
+    progress: function () {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        var slideProgress = swiper.slides[i].progress;
+        var innerOffset = swiper.width * interleaveOffset;
+        var innerTranslate = slideProgress * innerOffset;
+        swiper.slides[i].querySelector(".slide-inner").style.transform =
+          "translate3d(" + innerTranslate + "px, 0, 0)";
       }
+    },
 
-      function navigateUp() {
-        if (curPage === 1) return;
-        curPage--;
-        pagination();
+    touchStart: function () {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = "";
       }
+    },
 
-      function navigateDown() {
-        if (curPage === numOfPages) return;
-        curPage++;
-        pagination();
+    setTransition: function (speed) {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".slide-inner").style.transition =
+          speed + "ms";
       }
+    },
+  },
+};
 
-      $(document).on("mousewheel DOMMouseScroll", function (e) {
-        if (scrolling) return;
-        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-          navigateUp();
-        } else {
-          navigateDown();
-        }
-      });
+var swiper = new Swiper(".swiper-container", swiperOptions);
 
-      $(document).on("keydown", function (e) {
-        if (scrolling) return;
-        if (e.which === 38) {
-          navigateUp();
-        } else if (e.which === 40) {
-          navigateDown();
-        }
-      });
-    });
+// DATA BACKGROUND IMAGE
+var sliderBgSetting = $(".slide-bg-image");
+sliderBgSetting.each(function (indx) {
+  if ($(this).attr("data-background")) {
+    $(this).css("background-image", "url(" + $(this).data("background") + ")");
+  }
+});
--- portfolio/js/main_page.js
+++ portfolio/js/main_page.js
@@ -1,66 +1,150 @@
+// bottom line
+gsap.to(".bg_box ul ", {
+  duration: 1.5,
+  width: 100 + "%",
+  ease: "none",
+  // delay: 3,
+});
+// right line
+gsap.to(".bg_box ul li:nth-child(1)", {
+  duration: 0.7,
+  height: 35 + "vw",
+  opacity: "1",
+  ease: "none",
+  delay: 1.4,
+});
+gsap.to(".bg_box ul li:nth-child(2)", {
+  duration: 0.7,
+  height: 35 + "vw",
+  opacity: "1",
+  ease: "none",
+  delay: 1.6,
+});
+gsap.to(".bg_box ul li:nth-child(3)", {
+  duration: 0.7,
+  height: 35 + "vw",
+  opacity: "1",
+  ease: "none",
+  delay: 1.8,
+});
+gsap.to(".bg_box ul li:nth-child(4)", {
+  duration: 0.7,
+  height: 35 + "vw",
+  opacity: "1",
+  ease: "none",
+  delay: 2,
+});
+//라인
 
-// banner 움직이는 효과
-{
-const circle = document.querySelector(".cursor").getBoundingClientRect();
+// h1
+gsap.from(".main_page h1 span span ", {
+  delay: 2,
+  y: 200,
+  stagger: {
+    amount: 0.6,
+    // from: "end",
+  },
+});
+setTimeout(() => {
+  gsap.to(".main_page h1 span span ", {
+    y: 0,
+    delay: 2.5,
+  });
+}, 4100);
 
-document.querySelector(".main_inner").addEventListener("mousemove", (e)=>{
-     //커서
-     gsap.to(".cursor", {duration: .2, left: e.pageX - circle.width/2, top: e.pageY - circle.height/2});
+// h2
+gsap.from(".main_page h2 span span ", {
+  delay: 2.2,
+  y: 200,
+  stagger: {
+    amount: 0.6,
+    // from: "center",
+  },
+});
+setTimeout(() => {
+  gsap.to(".main_page h2 span span ", {
+    y: 0,
+    delay: 2.7,
+  });
+}, 4100);
 
-     //마우스 좌표값
-     let mousePageX = e.pageX
-     let mousePageY = e.pageY
+gsap.to(".sub_text", {
+  opacity: 1,
+  duration: 1,
+  delay: 2.9,
+});
+gsap.to(".main_inpo", {
+  opacity: 1,
+  duration: 1.5,
+  delay: 2.9,
+});
+gsap.to(".main_inner", {
+  opacity: 1,
+  duration: 2,
+  delay: 3,
+});
 
-     //마우스 좌표값을 가운데 초기화
-     let centerPageX = window.innerWidth/2 - mousePageX
-     let centerPageY = window.innerHeight/2 - mousePageY
+gsap.to("#about_bg", {
+  opacity: 1,
+  duration: 1,
+  delay: 1,
+});
 
-     //이미지 움직이기
-     gsap.to(".main_inner figure img", {duration: 0.3, x: centerPageX/20, y: centerPageY/20});
+var container;
+var camera, scene, renderer;
+var uniforms;
 
-     let windowHeight = window.screen.height //1080 // 화면 크기
+function init() {
+  container = document.getElementById("about_bg");
 
- })
- getSite = () => {
-    setTimeout(() => {
-        // .main__inner > div:nth-child(1)
-        gsap.to("#header", {
-            duration:0.5, 
-            top:0,
-            ease: "expo.out",
-            delay: 2.2
-        })
-        gsap.to(".sub_text", {
-            left:0,
-            opacity: 1,
-            duration:2,
-            delay: 2
-        })
-        gsap.to(".main_page h1 i", {
-            bottom:0,
-            opacity: 1,
-            duration:2,
-            delay: 1
+  camera = new THREE.Camera();
+  camera.position.z = 1;
 
-        })
-        gsap.to(".main_page h1 span", {
-            bottom:0,
-            opacity: 1,
-            duration: 2,
-            delay: 1.2
-        })
-        gsap.to(".main_inpo p", {
-            top:0,
-            opacity: 1,
-            duration: 1,
-            delay: 2.5
-        })
-        // gsap.to(".main_banner", {
-        //     opacity: 0,
-        //     duration: 0,
-        //     delay: 0
-        // })
-    }, 1000);
+  scene = new THREE.Scene();
+
+  var geometry = new THREE.PlaneBufferGeometry(2, 2);
+
+  uniforms = {
+    time: {
+      type: "f",
+      value: 1.0,
+    },
+    resolution: {
+      type: "v2",
+      value: new THREE.Vector2(),
+    },
+  };
+
+  var material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+  });
+
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  onWindowResize();
+  window.addEventListener("resize", onWindowResize, false);
 }
-getSite ()
+
+function onWindowResize(event) {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  uniforms.resolution.value.x = renderer.domElement.width;
+  uniforms.resolution.value.y = renderer.domElement.height;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  uniforms.time.value += 0.07;
+  renderer.render(scene, camera);
 }
