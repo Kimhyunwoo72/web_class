e4840110c129833baab5e6b7da3b2752d8b83416
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-05-19 15:00:45 +0900

    0519

--- portfolio3/js/game.js
+++ portfolio3/js/game.js
@@ -1,31 +1,4 @@
 gsap.registerPlugin(ScrollTrigger);
-// gsap.defaults({ ease: "power1", duration: 6 });
-
-// gsap.to(".gmae_info1", {
-//   scrollTrigger: {
-//     trigger: ".gmae_wrap",
-//     markers: true,
-//     start: "center 35%",
-//     scrub: true,
-//     pin: true,
-//   },
-//   x: -100 + "%",
-//   duration: 5,
-//   // anticipatePin: 1,
-// });
-
-// gsap.to(".gmae_info2", {
-//   scrollTrigger: {
-//     trigger: ".gmae_wrap",
-//     markers: true,
-//     start: "center 35%",
-//     scrub: true,
-//     pin: true,
-//   },
-//   x: -200 + "%",
-//   duration: 10,
-//   // anticipatePin: 3,
-// });
 
 let SECTIONS = gsap.utils.toArray(".gmae_info");
 
--- portfolio3/js/site.js
+++ portfolio3/js/site.js
@@ -1,19 +1,165 @@
-var windowWidth = window.innerWidth;
-gsap.from(".site_wrap", {
-  scrollTrigger: {
-    trigger: ".site_info",
-    markers: true,
-    start: "center center",
-  },
+gsap.from(".site_desc", {
   y: 100,
 });
-gsap.to(".site_wrap", {
+gsap.to(".site_desc", {
   scrollTrigger: {
-    trigger: ".site_info",
+    trigger: ".site_wrap",
     markers: true,
-    start: "center center",
+    start: "top center",
   },
-  opacity: 1,
   y: 0,
+  ease: "sine.out",
+  opacity: 1,
   duration: 1.5,
 });
+
+///////////////////////
+// line1
+gsap.from(".site_line1", {
+  scaleX: 0,
+});
+gsap.to(".site_line1", {
+  scrollTrigger: {
+    trigger: ".site_wrap",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+});
+
+// 2
+// line2
+gsap.from(".site_line2", {
+  scaleX: 0,
+});
+gsap.to(".site_line2", {
+  scrollTrigger: {
+    trigger: ".site_wrap",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+  delay: 0.2,
+});
+
+// line3
+gsap.from(".site_line3", {
+  scaleX: 0,
+});
+gsap.to(".site_line3", {
+  scrollTrigger: {
+    trigger: ".site_wrap",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+  delay: 0.4,
+});
+
+// line4
+gsap.from(".site_line4", {
+  scaleX: 0,
+});
+gsap.to(".site_line4", {
+  scrollTrigger: {
+    trigger: ".site_wrap",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+  delay: 0.9,
+});
+
+// Height line
+gsap.to(".site_info1 .siteH_line", {
+  scrollTrigger: {
+    trigger: ".site_wrap",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  duration: 2,
+  delay: 0.3,
+  height: 100 + "%",
+});
+// 1
+
+// 2
+// line2
+gsap.from(".site_line2", {
+  scaleX: 0,
+});
+gsap.to(".site_line2", {
+  scrollTrigger: {
+    trigger: ".site_info2",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+  delay: 0.2,
+});
+
+// line3
+gsap.from(".site_line3", {
+  scaleX: 0,
+});
+gsap.to(".site_line3", {
+  scrollTrigger: {
+    trigger: ".site_info2",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+  delay: 0.4,
+});
+
+// line4
+gsap.from(".site_line4", {
+  scaleX: 0,
+});
+gsap.to(".site_line4", {
+  scrollTrigger: {
+    trigger: ".site_info2",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  scaleX: 1,
+  duration: 2,
+  delay: 0.9,
+});
+
+// Height line
+gsap.to(".site_info2 .siteH_line", {
+  scrollTrigger: {
+    trigger: ".site_info2",
+    markers: true,
+    start: "top center",
+  },
+  ease: "power2.out",
+  opacity: 1,
+  duration: 2,
+  delay: 0.3,
+  height: 100 + "%",
+});
+// 2
