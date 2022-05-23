6dda567eb5c2600897d2d2794ec080f12a936d25
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-05-23 10:50:01 +0900

    0523

--- portfolio/js/main_page.js
+++ portfolio/js/main_page.js
@@ -1,83 +1,86 @@
-// bottom line
-gsap.to(".bg_box ul ", {
-  duration: 1.5,
-  width: 100 + "%",
-  ease: "none",
-  // delay: 3,
-});
-// right line
-gsap.to(".bg_box ul li:nth-child(1)", {
-  duration: 0.7,
-  height: 35 + "vw",
-  opacity: "1",
-  ease: "none",
-  delay: 1.4,
-});
-gsap.to(".bg_box ul li:nth-child(2)", {
-  duration: 0.7,
-  height: 35 + "vw",
-  opacity: "1",
-  ease: "none",
-  delay: 1.6,
-});
-gsap.to(".bg_box ul li:nth-child(3)", {
-  duration: 0.7,
-  height: 35 + "vw",
-  opacity: "1",
-  ease: "none",
-  delay: 1.8,
-});
-gsap.to(".bg_box ul li:nth-child(4)", {
-  duration: 0.7,
-  height: 35 + "vw",
-  opacity: "1",
-  ease: "none",
-  delay: 2,
-});
-//라인
-
-// h1
-gsap.from(".main_page h1 span span ", {
-  delay: 2,
-  y: 200,
-  stagger: {
-    amount: 0.6,
-    // from: "end",
-  },
-});
-setTimeout(() => {
-  gsap.to(".main_page h1 span span ", {
-    y: 0,
-    delay: 2.5,
+function main() {
+  // bottom line
+  gsap.to(".bg_box ul ", {
+    duration: 1.5,
+    width: 100 + "%",
+    ease: "none",
+    // delay: 3,
   });
-}, 4100);
+  // right line
+  gsap.to(".bg_box ul li:nth-child(1)", {
+    duration: 0.7,
+    height: 35 + "vw",
+    opacity: "1",
+    ease: "none",
+    delay: 1.4,
+  });
+  gsap.to(".bg_box ul li:nth-child(2)", {
+    duration: 0.7,
+    height: 35 + "vw",
+    opacity: "1",
+    ease: "none",
+    delay: 1.6,
+  });
+  gsap.to(".bg_box ul li:nth-child(3)", {
+    duration: 0.7,
+    height: 35 + "vw",
+    opacity: "1",
+    ease: "none",
+    delay: 1.8,
+  });
+  gsap.to(".bg_box ul li:nth-child(4)", {
+    duration: 0.7,
+    height: 35 + "vw",
+    opacity: "1",
+    ease: "none",
+    delay: 2,
+  });
+  //라인
 
-// h2
-gsap.from(".main_page h2 span span ", {
-  delay: 2.2,
-  y: 200,
-  stagger: {
-    amount: 0.6,
-    // from: "center",
-  },
-});
+  // h1
+  gsap.from(".main_page h1 span span ", {
+    delay: 2,
+    y: 200,
+    stagger: {
+      amount: 0.6,
+      // from: "end",
+    },
+  });
+  setTimeout(() => {
+    gsap.to(".main_page h1 span span ", {
+      y: 0,
+      delay: 2.5,
+    });
+  }, 4100);
 
-gsap.to(".main_inpo", {
-  opacity: 1,
-  duration: 1.5,
-  delay: 2.9,
-});
-gsap.to(".main_inner", {
-  opacity: 1,
-  duration: 2,
-  delay: 3.7,
-});
+  // h2
+  gsap.from(".main_page h2 span span ", {
+    delay: 2.2,
+    y: 200,
+    stagger: {
+      amount: 0.6,
+      // from: "center",
+    },
+  });
 
-gsap.to("#about_bg", {
-  opacity: 1,
-  duration: 1,
-  delay: 3,
-});
+  gsap.to(".main_inpo", {
+    opacity: 1,
+    duration: 1.5,
+    delay: 2.9,
+  });
+  gsap.to(".main_inner", {
+    opacity: 1,
+    duration: 2,
+    delay: 3.7,
+  });
+
+  gsap.to("#about_bg", {
+    opacity: 1,
+    duration: 1,
+    delay: 3,
+  });
+}
+// 함수끝
 
 var container;
 var camera, scene, renderer;
