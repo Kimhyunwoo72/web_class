b9bb0f4c112047d1f8e790ec0bdb329c6295079a
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-05-26 09:26:53 +0900

    20220526

---
+++ portfolio/effect/musicPlayer/api/music.js
@@ -0,0 +1,63 @@
+let allMusic = [
+    {
+        name: "1. Sailing",
+        artist : "Telecasted",
+        img : "music01",
+        audio : "music01"
+    },   
+    {
+        name: "2. Breeze",
+        artist : "Telecasted",
+        img : "music02",
+        audio : "music02"
+    },   
+    {
+        name: "3. Sunshine",
+        artist : "jone Mickal",
+        img : "music03",
+        audio : "music03"
+    },   
+    {
+        name: "4. Built to Last (Instrumental)",
+        artist : "NEFFEX",
+        img : "music04",
+        audio : "music04"
+    },   
+    {
+        name: "5. Hustlin' (Instrumental)",
+        artist : "NEFFEX",
+        img : "music05",
+        audio : "music05"
+    },   
+    {
+        name: "6. Bop Walker",
+        artist : "Freedom Trail Studio",
+        img : "music06",
+        audio : "music06"
+    },   
+    {
+        name: "7. Robots and Aliens",
+        artist : "Joel Cummins",
+        img : "music07",
+        audio : "music07"
+    },   
+    {
+        name: "8. Heal You",
+        artist : "Freedom Trail Studio",
+        img : "music08",
+        audio : "music08"
+    },   
+    {
+        name: "9. Tempos Vari",
+        artist : "TrackTribe",
+        img : "music09",
+        audio : "music09"
+    },   
+    {
+        name: "10. By the Fireplace",
+        artist : "TrackTribe",
+        img : "music10",
+        audio : "music10"
+    },   
+
+]
